1ede282d1d1871aace317d9032b5563877cf19d6
Anja Böttcher <anjabottcher@Anja-Bottchers-MacBook-Air.local>
2019-07-08 15:20:57 +0100

    updated user models

---
+++ models/Zens.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const zensSchema = new Schema({
+  _creator: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'User'
+  },
+  title: { type: String, required: true },
+  description: { type: String, required: true, maxlength: 280 },
+  links: [String],
+  images: String,
+  additional_info: String,   
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const Zens = mongoose.model("Zens", zensSchema);
+module.exports = Zens;
