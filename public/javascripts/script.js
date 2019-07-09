a64d7984d3c2de65afd3611ca9c76418d022c940
marianamv112 <marianamv112@gmail.com>
2019-07-09 18:33:08 +0100

    Some advance in create and update zens

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
-
-}, false);
-
+var index = 1;
+function add_link_field() {
+  index++;
+  var objTo = document.querySelector(".link_fields");
+  var divtest = document.createElement("div");
+  divtest.setAttribute("class", "form-group removeclass" + index);
+  var rdiv = "removeclass" + index;
+  divtest.innerHTML = '<div class="input-group md-form mt-0"><input class="form-control" name="links[]" type="text" placeholder="Type something" /><button class="btn btn-success btn-add" type="button" onclick="add_link_field()"><span>+</span></button><button class="btn btn-danger btn-add" type="button" onclick="remove_link_field('+index+')"><span>-<span></button></div>'
+  
+  objTo.appendChild(divtest);
+}
+function remove_link_field(rid) {
+  $(".removeclass" + rid).remove();
+}
--- routes/main.js
+++ routes/main.js
@@ -1,27 +1,112 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Zen = require('../models/Zen'); 
+const Zen = require("../models/Zen");
+const nodemailer = require("nodemailer");
 
-router.get('/zen-board', (req, res, next) => {
-    Zen.find({}).then(zens =>
-        
-        {res.render('zen-board', {zens})
-    });
+const multer = require("multer");
+const upload = multer({ dest: "./public/uploads" });
+
+router.get("/zen-board", (req, res, next) => {
+  Zen.find({}).then(zens => {
+    res.render("zen-board", { zens });
+  });
 });
 
-router.get('/zen-history', (req, res, next) => {
-    Zen.find({}).then(zens =>
-        
-        {res.render('zen-history', {zens})
-    });
+router.get("/zen-history", (req, res, next) => {
+  Zen.find({}).then(zens => {
+    res.render("zen-history", { zens });
+  });
 });
 
-router.get('/delete-zen/:zenId', (req, res, next) => {
-    let zenId = req.params.zenId
-    Zen.findOneAndDelete({_id: zenId}).then(response => {
-         res.redirect("/main/zen-history");
-    }); 
+router.get("/delete-zen/:zenId", (req, res, next) => {
+  let zenId = req.params.zenId;
+  Zen.findOneAndDelete({ _id: zenId }).then(response => {
+    res.redirect("/main/zen-history");
+  });
 });
 
+router.get("/edit-zen/:zenId", (req, res, next) => {
+  let zenId = req.params.zenId;
+  Zen.findOne({ _id: zenId }).then(zen => {
+    res.render("edit-zen", zen);
+  });
+});
+
+router.get("/create-zen", (req, res, next) => {
+  res.render("create-zen");
+});
+
+router.post("/send-zen", upload.single("image"), (req, res, next) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let additional_info = req.body.additional_info;
+  let image = req.file.filename;
+  let links = req.body.links;
+  let destination_email = req.body.destination;
+
+  Zen.create({
+    title: title,
+    description: description,
+    additional_info: additional_info,
+    image: `/uploads/${image}`,
+    links: links
+  })
+    .then(response => {
+      let transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASS
+        }
+      });
+
+      transporter.sendMail({
+        from: "My website",
+        to: destination_email,
+        subject: "A Zen from a Friend",
+        text: "Wtv"
+        //,"html": ``
+      });
+      res.redirect("/main/zen-history");
+    })
+    .catch(err => console.log(err));
+});
+
+router.post("/resend-zen", upload.single("image"), (req, res, next) => {
+  let title = req.body.title;
+  let description = req.body.description;
+  let additional_info = req.body.additional_info;
+  let image = req.file.filename;
+  let links = req.body.links;
+  let destination_email = req.body.destination;
+  let zenId = req.params.zenId;
+
+  Zen.findByIdAndUpdate(zenId, {
+    title: title,
+    description: description,
+    additional_info: additional_info,
+    image: `/uploads/${image}`,
+    links: links
+  })
+    .then(response => {
+      let transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASS
+        }
+      });
+
+      transporter.sendMail({
+        from: "My website",
+        to: destination_email,
+        subject: "A Zen from a Friend",
+        text: "Wtv"
+        //,"html": ``
+      });
+      res.redirect("/main/zen-history");
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
