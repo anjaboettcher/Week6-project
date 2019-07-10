cb0dd73d524c64e1696dfc52de88b9e9e25e855f
marianamv112 <marianamv112@gmail.com>
2019-07-10 13:01:11 +0100

    test

--- bin/seeds.js
+++ bin/seeds.js
@@ -11,7 +11,7 @@ const Zen = require("../models/Zen");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/project-w6', {useNewUrlParser: true})
+  .connect(MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
